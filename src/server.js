f220bce1afd2a356829b01717da59b5b0dc31197
liamcunniffe <liamcunniffe88@outlook.com>
2024-02-06 21:45:58 +0000

    enhancement:awsome example complete

--- src/server.js
+++ src/server.js
@@ -2,9 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/health", (req, res) => {
-  res.send("healthy");
-});
+app.use("/example", express.static("example"));
 
 app.listen(5001, () => {
   console.log("server is listening on port 5001");
