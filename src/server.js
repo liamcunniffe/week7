847b70cc4eea7325e42f70a2cdbf0a289299fbac
liamcunniffe <liamcunniffe88@outlook.com>
2024-02-08 19:04:58 +0000

    chore: cleanup

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-
 app.listen(5001, () => {
   console.log("server is listening on port 5001");
 });
